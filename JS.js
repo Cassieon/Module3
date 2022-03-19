e13ec81a25a1e6569a78477b3013df797a41fc95
Cassieon Craft <cass.m.craft@gmail.com>
2022-03-19 18:05:37 -0400

    chged names of docs to reflect project

--- JS.js
+++ JS.js
@@ -1,10 +1,74 @@
-function name(x) {
-    let firstName = "Cassieon";
-    let lastName = "Craft";
+// function name() {
+//     let firstName = "Cassieon";
+//     let lastName = "Craft";
 
-}
-let count = 0;
-while (count < 10) {
-    count++;
-    console.log(x);
-};
+// }
+// let count = 0;
+// while (count < 10) {
+//     count++;
+//     console.log();
+// };
+
+// let friends = ["Cat","Ray","Kalyn"];
+
+// for (let i = 0; i < friends.length; i++){
+//     console.log(friends[i]);
+// }
+
+// 
+
+// function isVowel(v) {
+//     const letter = ["a", "e", "i", "o", "u"];
+//     let result;
+
+//     if (v = letter) {
+//         result = true;
+//     } else {
+//     result = false;
+//     }
+//     return result;
+// }
+
+
+
+// console.log(isVowel("a")) // true  
+// console.log(isVowel("c"))// false
+// console.log(isVowel("e")) // true 
+
+// class Calzone {
+//     constructor(bread, meat, sauce, cheese, extra1, extra2,) {
+//         this.bread = bread;
+//         this.meat = meat;
+//         this.sauce = sauce;
+//         this.cheese = cheese;
+//         this.extra1 = extra1;
+//         this.extra2 = extra2;
+//     }
+//     changeCheese(diffCheese) {
+//         this.cheese = diffCheese;
+//     }
+//     changeMeat(noMeat) {
+//         this.meat = noMeat;
+//     }
+//     changeBread(pasta) {
+//         this.bread = pasta;
+//     }
+// }
+// const myCalzone = new Calzone("NonGluten", "chicken", "SpicyTomato", "Chedder", "mushrooms", "peppers");
+// myCalzone.changeCheese("mozzarella");
+// myCalzone.changeMeat("noMeat");
+
+
+// console.log(myCalzone);
+
+
+// function tellFortune(x, y, z, n) {
+//     var fortune = "you will be a " + x + "in " + y + " and married to " + z + " with " + n + " kids.";
+//     console.log(fortune)
+// }
+// tellFortune("musician", "DC", "Neal", 2);
+
+// function calculateDogAge(a) {
+//     return a + 7;
+// }
+// let x = "Your doggie is" + calculateDogAge(3) + "years old in dog years!";
